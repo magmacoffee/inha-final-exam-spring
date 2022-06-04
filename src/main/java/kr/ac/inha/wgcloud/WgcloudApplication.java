1b9cb9f13c865b4c41705ebe5e7eae315ae2f286
magmacoffee <magmacoffee@gmail.com>
2022-06-04 11:16:29 +0900

    init project

---
+++ src/main/java/kr/ac/inha/wgcloud/WgcloudApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.inha.wgcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WgcloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WgcloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/ac/inha/wgcloud/WgcloudApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.inha.wgcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WgcloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
