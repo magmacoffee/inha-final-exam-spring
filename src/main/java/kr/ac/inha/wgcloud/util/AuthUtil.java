1829fd30769046628d971b284c33ded2ae77c0cb
magmacoffee <magmacoffee@gmail.com>
2022-06-17 20:13:30 +0900

    add AuthUtil (for get login user id)

---
+++ src/main/java/kr/ac/inha/wgcloud/util/AuthUtil.java
@@ -0,0 +1,13 @@
+package kr.ac.inha.wgcloud.util;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class AuthUtil {
+
+    public static String getLoginUserId() {
+        UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        return userDetails.getUsername();
+    }
+
+}
