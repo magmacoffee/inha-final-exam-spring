babe02ae5cf2fbfd362554df8916fd3f18a2d703
magmacoffee <magmacoffee@gmail.com>
2022-06-17 22:08:48 +0900

    add FileRepository

--- src/main/java/kr/ac/inha/wgcloud/file/repository/FileRepository.java
+++ src/main/java/kr/ac/inha/wgcloud/file/repository/FileRepository.java
@@ -1,8 +1,9 @@
 package kr.ac.inha.wgcloud.file.repository;
 
+import kr.ac.inha.wgcloud.file.vo.FileVo;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface FileRepository {
-
+    void insertFile(FileVo file) throws Exception;
 }
