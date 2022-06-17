bf10799be78f98f6b75236f890fdba0b0eaf627b
magmacoffee <magmacoffee@gmail.com>
2022-06-18 02:36:17 +0900

    add get file list api

--- src/main/java/kr/ac/inha/wgcloud/file/repository/FileRepository.java
+++ src/main/java/kr/ac/inha/wgcloud/file/repository/FileRepository.java
@@ -3,7 +3,10 @@ package kr.ac.inha.wgcloud.file.repository;
 import kr.ac.inha.wgcloud.file.vo.FileVo;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface FileRepository {
+    List<FileVo> selectFileList(int empId) throws Exception;
     void insertFile(FileVo file) throws Exception;
 }
