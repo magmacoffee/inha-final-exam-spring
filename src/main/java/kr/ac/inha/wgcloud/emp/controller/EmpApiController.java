f1edc6696f436ca79377068e3840f9d130eff50b
magmacoffee <magmacoffee@gmail.com>
2022-06-19 18:18:18 +0900

    add getEmp api

--- src/main/java/kr/ac/inha/wgcloud/emp/controller/EmpApiController.java
+++ src/main/java/kr/ac/inha/wgcloud/emp/controller/EmpApiController.java
@@ -1,9 +1,13 @@
 package kr.ac.inha.wgcloud.emp.controller;
 
+import kr.ac.inha.wgcloud.common.ApiErrorCode;
+import kr.ac.inha.wgcloud.common.ApiException;
 import kr.ac.inha.wgcloud.emp.service.EmpService;
+import kr.ac.inha.wgcloud.emp.vo.Emp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,6 +28,12 @@ public class EmpApiController {
     public ResponseEntity<?> getLoginEmp(Principal principal) throws Exception {
         String id = principal.getName();
         return ResponseEntity.ok(empService.getEmpById(id));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getEmp(@PathVariable String id) throws Exception {
+        Emp emp = empService.getEmpById(id);
+        return ResponseEntity.ok(emp);
     }
 
 }
