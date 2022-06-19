aeda106892726e5e0ffcc00a623145262bb30e19
magmacoffee <magmacoffee@gmail.com>
2022-06-19 15:19:56 +0900

    ShareFileVo change field seq

---
+++ src/main/java/kr/ac/inha/wgcloud/file/vo/ShareFileVo.java
@@ -0,0 +1,30 @@
+package kr.ac.inha.wgcloud.file.vo;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@Builder
+@ToString
+public class ShareFileVo {
+    private int dirId;
+    private String groupId;
+    private String empId;
+    private String rootDirId;
+    private String name;
+    private String orgFileName;
+    private String ext;
+    private long fileSize;
+    private String filePath;
+    private String sharedEmpId;
+    private boolean isFile;
+    private int downCount;
+    private boolean isDeleted;
+    private String createDateTime;
+    private String updateDateTime;
+    private String shareDateTime;
+    private String shareEmpName; // new field
+}
